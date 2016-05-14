5fddcb4cf4f3d2941c4a6c2c37a0976646f01869
Anze Kolar <me@akolar.com>
2016-05-14 15:01:52 +0200

    Add templates

--- src/public/index.php
+++ src/public/index.php
@@ -1,14 +1,37 @@
 <?php
+// {{{ Slim config
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
 require '../../vendor/autoload.php';
 
-$app = new \Slim\App;
-$app->get('/hello/{name}', function (Request $request, Response $response) {
-    $name = $request->getAttribute('name');
-    $response->getBody()->write("Hello, $name");
+$config['displayErrorDetails'] = true;
 
-    return $response;
-});
+$app = new \Slim\App(["settings" => $config]);
+
+// Get container
+$container = $app->getContainer();
+
+// Register component on container
+$container['view'] = function ($container) {
+    $view = new \Slim\Views\Twig('../templates', [
+        'cache' => '../cache'
+    ]);
+    $view->addExtension(new \Slim\Views\TwigExtension(
+        $container['router'],
+        $container['request']->getUri()
+    ));
+
+    return $view;
+};
+// }}}
+
+// {{{ Main
+$app->get('/hello/{name}', function ($request, $response, $args) {
+    return $this->view->render($response, 'profile.html', [
+        'name' => $args['name']
+    ]);
+})->setName('profile');
+// }}}
+
 $app->run();
